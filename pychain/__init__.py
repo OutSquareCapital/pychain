245192ea017a9db32c59ff2817f621322e5cc3a7
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-07-05 16:43:16 +0200

    added partial funcs for convenience

--- pychain/__init__.py
+++ pychain/__init__.py
@@ -5,11 +5,12 @@ from pychain.main import (
     from_scalar,
     from_range,
 )
-
+from functools import partial
 __all__: list[str] = [
     "from_scalar",
     "from_iterable",
     "from_dict",
     "from_dict_of_iterables",
     "from_range",
+    "partial",
 ]
