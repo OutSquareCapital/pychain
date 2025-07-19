44a5a61bbc613d4436f0a54a7f2fa1fc87ca4aba
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-07-19 13:11:30 +0200

    added numba method for additional JIT compilation capabilities

--- src/pychain/_func.py
+++ src/pychain/_func.py
@@ -7,20 +7,17 @@ from typing import Any
 @dataclass(slots=True, frozen=True, repr=False)
 class Func[P, R]:
     """
-    A callable object that represents a compiled function with source code.
-
-    The actual function is a private attribute. 
+    A frozen dataclass that represents a compiled function with source code.
 
     This allows you to take advantage of this class for development purposes, and then extract the function for production deployments.
 
-    The 5-10% speed up may or may not be useful, but you have the choice!
-
-    Provides: 
+    Provides:
         - A repr for easy debugging
         - A convenient way to type hint the function(without having to import from collections.abc.... and the long typing syntax)
-        - An `extract` method to return the wrapped func as an ultimate speed up. Use it for production code.
-
+        - A `numbify` method to compile the function with numba for potential performance improvements.
+        - An `extract` method to return the wrapped func. Use it for production code.
     """
+
     _compiled_func: Callable[[P], R]
     _source_code: str
 
@@ -31,6 +28,15 @@ class Func[P, R]:
         signature: str = self._source_code.splitlines()[0]
         indented_code: str = textwrap.indent(self._source_code, "    ")
         return f"pychain.Func({signature})\n-- Source --\n{indented_code}"
+
+    def numbify(self) -> "Func[P, R]":
+        from numba import jit
+        try:
+            compiled_func: Callable[[P], R] = jit(self._compiled_func)
+        except Exception as e:
+            print(f"Failed to compile function: {e}")
+            return self
+        return Func(compiled_func, self._source_code)
 
     def extract(self) -> Callable[[P], R]:
         """
