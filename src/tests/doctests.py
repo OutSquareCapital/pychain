9223f26ae656e7b1d011e8a3f1b885956618c2b9
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-07-19 18:30:36 +0200

    cleaning C code

--- src/tests/doctests.py
+++ src/tests/doctests.py
@@ -1,58 +1,10 @@
-import ast
 import doctest
 import importlib
 import pkgutil
 import sys
-from pathlib import Path
 from types import ModuleType
 
 from tqdm import tqdm
-
-
-def _test_stub_files(package: str) -> None:
-    package_path = Path(importlib.import_module(package).__path__[0])
-    stub_files = package_path.rglob("*.pyi")
-    failures = 0
-    from pychain import Iter, Struct, expr
-
-    globs = {
-        "Expr": expr,
-        "Iter": Iter,
-        "Struct": Struct
-    }
-
-    for stub_file in stub_files:
-        with open(stub_file, "r+", encoding="utf-8") as f:
-            content = f.read()
-            new_content: str = content.replace('default[V]="foo"', 'default="foo"')
-            if content != new_content:
-                f.seek(0)
-                f.write(new_content)
-                f.truncate()
-                content: str = new_content
-
-        try:
-            tree = ast.parse(content)
-            for node in ast.walk(tree):
-                if isinstance(node, (ast.FunctionDef, ast.ClassDef, ast.Module)):
-                    if docstring := ast.get_docstring(node):
-                        parser = doctest.DocTestParser()
-                        test = parser.get_doctest(
-                            string=docstring,
-                            globs=globs,
-                            name=node.name,  # type: ignore
-                            filename=str(object=stub_file),
-                            lineno=node.lineno,  # type: ignore
-                        )
-                        runner = doctest.DocTestRunner()
-                        failures += runner.run(test).failed
-        except Exception as e:
-            print(f"Error testing {stub_file}: {e}")
-
-    if failures > 0:
-        print(f"\nSome doctests in stubs failed. ❌ ({failures} failures)")
-        sys.exit(1)
-
 
 def _get_modules(package: str) -> list[ModuleType]:
     modules: list[ModuleType] = []
@@ -83,7 +35,6 @@ def _test_modules(modules: list[ModuleType]) -> None:
 def run_all_doctests(package: str) -> None:
     modules: list[ModuleType] = _get_modules(package)
     _test_modules(modules)
-    _test_stub_files(package)
     print("\nAll doctests passed! ✅")
 
 
