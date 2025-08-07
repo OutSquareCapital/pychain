2b83120ece37f39f25c5cfbcc1b505b75d233b59
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-08-07 12:35:21 +0200

    moved the cytoolz stubs as an external dependency

--- typings/cytoolz/__init__.pyi
+++
@@ -1,6 +0,0 @@
-import cytoolz.dicttoolz as dicttoolz
-import cytoolz.itertoolz as itertoolz
-import cytoolz.functoolz as functoolz
-import cytoolz.recipes as recipes
-
-__all__ = ["dicttoolz", "itertoolz", "functoolz", "recipes"]
