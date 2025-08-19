95ddd595de113ac1416e36210da6e36f22494893
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-08-19 16:11:12 +0200

    deleted all lazy wip dependencies and related code

--- src/pychain/_lazy/_compilers/__init__.py
+++
@@ -1,4 +0,0 @@
-from ._ast_parsers import TypedLambda, TypeTracker
-from ._lib import Compiler
-
-__all__ = ["TypeTracker", "TypedLambda", "Compiler"]
--- src/pychain/_lazy/_expressions/__init__.py
+++
@@ -1,5 +0,0 @@
-from ._exprs import Expr
-from ._iter import LazyIter
-from ._struct import LazyStruct
-
-__all__ = ["Expr", "LazyIter", "LazyStruct"]
