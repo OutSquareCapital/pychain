0a8f392355c2de3d3aa52ba61a22439054fd3a53
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-09-14 06:30:45 +0200

    added a few methods

--- src/pychain/_iter/_process.py
+++ src/pychain/_iter/_process.py
@@ -4,6 +4,7 @@ from random import Random
 from typing import Any, Concatenate, Self
 
 import cytoolz as cz
+import more_itertools as mit
 
 from .. import _core as core
 
@@ -245,3 +246,20 @@ class IterProcess[T](core.CommonBase[Iterable[T]]):
             [1, 2, 3]
         """
         return self._new(cz.itertoolz.concat((self._data, *others)))
+
+    def filter_except(
+        self, func: core.Transform[T, object], *exceptions: type[BaseException]
+    ) -> Self:
+        """Yield the items from iterable for which the validator function does not raise one of the specified exceptions.
+
+        validator is called for each item in iterable. It should be a function that accepts one argument and raises an exception if that item is not valid.
+
+        If an exception other than one given by exceptions is raised by validator, it is raised like normal.
+
+        **Example:**
+        >>> from pychain import Iter
+        >>> iterable = ["1", "2", "three", "4", None]
+        >>> Iter(iterable).filter_except(int, ValueError, TypeError).to_list()
+        ['1', '2', '4']
+        """
+        return self._new(mit.filter_except(func, self._data, *exceptions))
