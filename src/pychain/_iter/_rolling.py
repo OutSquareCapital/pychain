b667454886eeb1e4c8d118a4022040daceb4d13e
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-09-18 11:04:45 +0200

    renamed _data to _parent attribute in rolling to align it with others namespaces conventions

--- src/pychain/_iter/_rolling.py
+++ src/pychain/_iter/_rolling.py
@@ -14,7 +14,7 @@ if TYPE_CHECKING:
 
 @dataclass(slots=True)
 class RollingNameSpace[T]:
-    _data: Iterable[T]
+    _parent: Iterable[T]
     _window: int
 
     def mean(self) -> Iter[float]:
@@ -25,7 +25,7 @@ class RollingNameSpace[T]:
         >>> Iter([1, 2, 3, 4, 5]).rolling(3).mean().to_list()
         [2.0, 3.0, 4.0]
         """
-        return iter_factory(rolling.Mean(self._data, self._window))
+        return iter_factory(rolling.Mean(self._parent, self._window))
 
     def median(self) -> Iter[T]:
         """
@@ -35,7 +35,7 @@ class RollingNameSpace[T]:
         >>> Iter([1, 3, 2, 5, 4]).rolling(3).median().to_list()
         [2, 3, 4]
         """
-        return iter_factory(rolling.Median(self._data, self._window))
+        return iter_factory(rolling.Median(self._parent, self._window))
 
     def sum(self) -> Iter[T]:
         """
@@ -47,7 +47,7 @@ class RollingNameSpace[T]:
         >>> Iter([1.0, 2, 3, 4, 5]).rolling(3).sum().to_list()
         [6.0, 9.0, 12.0]
         """
-        return iter_factory(rolling.Sum(self._data, self._window))
+        return iter_factory(rolling.Sum(self._parent, self._window))
 
     def min(self) -> Iter[T]:
         """
@@ -57,7 +57,7 @@ class RollingNameSpace[T]:
         >>> Iter([3, 1, 4, 1, 5, 9, 2]).rolling(3).min().to_list()
         [1, 1, 1, 1, 2]
         """
-        return iter_factory(rolling.Min(self._data, self._window))
+        return iter_factory(rolling.Min(self._parent, self._window))
 
     def max(self) -> Iter[T]:
         """
@@ -67,7 +67,7 @@ class RollingNameSpace[T]:
         >>> Iter([3, 1, 4, 1, 5, 9, 2]).rolling(3).max().to_list()
         [4, 4, 5, 9, 9]
         """
-        return iter_factory(rolling.Max(self._data, self._window))
+        return iter_factory(rolling.Max(self._parent, self._window))
 
     def var(self) -> Iter[float]:
         """
@@ -77,7 +77,7 @@ class RollingNameSpace[T]:
         >>> Iter([1, 2, 4, 1, 4]).rolling(3).var().map(lambda x: round(x, 2)).to_list()
         [2.33, 2.33, 3.0]
         """
-        return iter_factory(rolling.Var(self._data, self._window))
+        return iter_factory(rolling.Var(self._parent, self._window))
 
     def std(self) -> Iter[float]:
         """
@@ -87,7 +87,7 @@ class RollingNameSpace[T]:
         >>> Iter([1, 2, 4, 1, 4]).rolling(3).std().map(lambda x: round(x, 2)).to_list()
         [1.53, 1.53, 1.73]
         """
-        return iter_factory(rolling.Std(self._data, self._window))
+        return iter_factory(rolling.Std(self._parent, self._window))
 
     def kurtosis(self) -> Iter[float]:
         """
@@ -98,7 +98,7 @@ class RollingNameSpace[T]:
         >>> Iter([1, 2, 4, 1, 4]).rolling(4).kurtosis().to_list()
         [1.5, -3.901234567901234]
         """
-        return iter_factory(rolling.Kurtosis(self._data, self._window))
+        return iter_factory(rolling.Kurtosis(self._parent, self._window))
 
     def skew(self) -> Iter[float]:
         """
@@ -109,7 +109,7 @@ class RollingNameSpace[T]:
         >>> Iter([1, 2, 4, 1, 4]).rolling(3).skew().map(lambda x: round(x, 2)).to_list()
         [0.94, 0.94, -1.73]
         """
-        return iter_factory(rolling.Skew(self._data, self._window))
+        return iter_factory(rolling.Skew(self._parent, self._window))
 
     def all(self) -> Iter[bool]:
         """
@@ -119,7 +119,7 @@ class RollingNameSpace[T]:
         >>> Iter([True, True, False, True, True]).rolling(2).all().to_list()
         [True, False, False, True]
         """
-        return iter_factory(rolling.All(self._data, self._window))
+        return iter_factory(rolling.All(self._parent, self._window))
 
     def any(self) -> Iter[bool]:
         """
@@ -129,7 +129,7 @@ class RollingNameSpace[T]:
         >>> Iter([True, True, False, True, True]).rolling(2).any().to_list()
         [True, True, True, True]
         """
-        return iter_factory(rolling.Any(self._data, self._window))
+        return iter_factory(rolling.Any(self._parent, self._window))
 
     def product(self) -> Iter[float]:
         """
@@ -139,7 +139,7 @@ class RollingNameSpace[T]:
         >>> Iter([1, 2, 3, 4, 5]).rolling(3).product().to_list()
         [6.0, 24.0, 60.0]
         """
-        return iter_factory(rolling.Product(self._data, self._window))
+        return iter_factory(rolling.Product(self._parent, self._window))
 
     def apply[R](self, func: Callable[[Iterable[T]], R]) -> Iter[R]:
         """
@@ -154,7 +154,7 @@ class RollingNameSpace[T]:
         >>> Iter([1, 3, 2, 5, 4]).rolling(3).apply(range_func).to_list()
         [2, 3, 3]
         """
-        return iter_factory(rolling.Apply(self._data, self._window, "fixed", func))
+        return iter_factory(rolling.Apply(self._parent, self._window, "fixed", func))
 
     def apply_pairwise[R](
         self, other: Iterable[T], func: Callable[[T, T], R]
@@ -172,5 +172,5 @@ class RollingNameSpace[T]:
         [1.0, 0.0, -1.0]
         """
         return iter_factory(
-            rolling.ApplyPairwise(self._data, other, self._window, func)
+            rolling.ApplyPairwise(self._parent, other, self._window, func)
         )
