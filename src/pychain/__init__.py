22ac66b19791ba4f9cb857afd9e7403db4bd9089
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-09-19 18:10:08 +0200

    forgot to add iter on as public function

--- src/pychain/__init__.py
+++ src/pychain/__init__.py
@@ -1,5 +1,5 @@
 from ._arrays import Array
-from ._constructors import dict_zip, iter_count, iter_func, iter_range
+from ._constructors import dict_zip, iter_count, iter_func, iter_on, iter_range
 from ._dict import Dict
 from ._iter import Iter
 
@@ -11,4 +11,5 @@ __all__ = [
     "iter_func",
     "iter_range",
     "dict_zip",
+    "iter_on",
 ]
