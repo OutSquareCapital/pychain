8a9cd089e5aa60b21d21ddb4c0d54529e1d491fe
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-09-23 15:42:42 +0200

    deleting IO functions to avoid maintaining code for which the exacts needs are unclear ATM

--- src/pychain/_dict/_constructors.py
+++ src/pychain/_dict/_constructors.py
@@ -1,34 +1,7 @@
 from collections.abc import Iterable
-from pathlib import Path
 from typing import Any
 
 from ._main import Dict
-
-
-def read_json(filepath: Path | str) -> Dict[Any, Any]:
-    import json
-
-    return Dict(json.loads(Path(filepath).read_text(encoding="utf-8")))
-
-
-def read_csv(filepath: Path | str) -> Dict[str, list[str]]:
-    import csv
-
-    with open(filepath, newline="", encoding="utf-8") as csvfile:
-        reader = csv.reader(csvfile)
-    return Dict({rows[0]: rows[1:] for rows in reader})
-
-
-def read_toml(filepath: Path | str) -> Dict[str, Any]:
-    import tomllib
-
-    return Dict(tomllib.loads(Path(filepath).read_text()))
-
-
-def read_pickle(filepath: Path | str) -> Dict[Any, Any]:
-    import pickle
-
-    return Dict(pickle.loads(Path(filepath).read_bytes()))
 
 
 def dict_zip[K, V](keys: Iterable[K], values: Iterable[V]) -> Dict[K, V]:
