1748d60663586666df852f571b03169f0bf113c0
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-09-29 16:11:04 +0200

    deleted narwhals dependency

--- src/pychain/_iter/_constructors.py
+++ src/pychain/_iter/_constructors.py
@@ -5,7 +5,6 @@ from collections.abc import Callable
 from typing import TYPE_CHECKING
 
 import cytoolz as cz
-from narwhals.typing import IntoDataFrame, IntoLazyFrame
 
 from .._core import iter_factory
 
@@ -60,45 +59,6 @@ class IterConstructors:
         [0, 1, 2]
         """
         return iter_factory(cz.itertoolz.iterate(func, x))
-
-    @staticmethod
-    def from_col(data: IntoDataFrame | IntoLazyFrame, column: str):
-        """
-        Create an iterator from a column in a DataFrame or LazyFrame.
-
-        Anything supported by narwhals will works (e.g., pandas, polars, DuckDB, etc...).
-
-        Note that a LazyFrame input will be collected, but will only materialize the specified column.
-
-        >>> from pychain import Iter
-        >>> import polars as pl
-        >>> data = {"a": [1, 2, 3], "b": ["x", "y", "z"]}
-        >>> pl.LazyFrame(data).pipe(Iter.from_col, "a").to_list()
-        [1, 2, 3]
-
-        **Note for polars users**:
-
-        This mostly give you a convenient way to avoid interrupting the method chain when going from polars to pychain.
-
-        This method is simply syntactic sugar for:
-
-        `Iter.from_col(df.lazy().select(column).collect().get_column(column))``
-
-        This is due to the fact that polars.Series.pipe don't (and won't) exist.
-
-        For more details, see https://github.com/pola-rs/polars/issues/14032
-
-        """
-        import narwhals as nw
-
-        return iter_factory(
-            nw.from_native(data)
-            .lazy()
-            .select(column)
-            .collect()
-            .get_column(column)
-            .to_native()
-        )
 
     @staticmethod
     def from_[T](*elements: T) -> Iter[T]:
