e49767fd9dfa358952fa558f4ac27f4c05550f80
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-10-06 03:32:10 +0200

    new booleans module separated from aggregations -  added is empty check

--- src/pychain/_iter/_aggregations.py
+++ src/pychain/_iter/_aggregations.py
@@ -26,36 +26,6 @@ class IterAgg[T](CommonBase[Iterable[T]]):
         """
         return functools.reduce(func, self._data)
 
-    def is_distinct(self) -> bool:
-        """
-        Return True if all items are distinct.
-
-            >>> from pychain import Iter
-            >>> Iter([1, 2]).is_distinct()
-            True
-        """
-        return cz.itertoolz.isdistinct(self._data)
-
-    def all(self) -> bool:
-        """
-        Return True if all items are truthy.
-
-            >>> from pychain import Iter
-            >>> Iter([1, True]).all()
-            True
-        """
-        return all(self._data)
-
-    def any(self) -> bool:
-        """
-        Return True if any item is truthy.
-
-            >>> from pychain import Iter
-            >>> Iter([0, 1]).any()
-            True
-        """
-        return any(self._data)
-
     def first(self) -> T:
         """
         Return the first element.
@@ -107,38 +77,6 @@ class IterAgg[T](CommonBase[Iterable[T]]):
         """
         return cz.itertoolz.nth(index, self._data)
 
-    def all_equal[U](self, key: Callable[[T], U] | None = None) -> bool:
-        """
-        Return True if all items are equal.
-
-            >>> from pychain import Iter
-            >>> Iter([1, 1, 1]).all_equal()
-            True
-        """
-        return mit.all_equal(self._data, key=key)
-
-    def all_unique[U](self, key: Callable[[T], U] | None = None) -> bool:
-        """
-        Returns True if all the elements of iterable are unique (no two elements are equal).
-
-        >>> from pychain import Iter
-        >>> Iter("ABCB").all_unique()
-        False
-
-        If a key function is specified, it will be used to make comparisons.
-
-        >>> Iter("ABCb").all_unique()
-        True
-
-        >>> Iter("ABCb").all_unique(str.lower)
-        False
-
-        The function returns as soon as the first non-unique element is encountered.
-        Iterables with a mix of hashable and unhashable items can be used, but the function will be slower for unhashable items
-
-        """
-        return mit.all_unique(self._data, key=key)
-
     def argmax[U](self, key: Callable[[T], U] | None = None) -> int:
         """
         Index of the first occurrence of a maximum value in an iterable.
@@ -189,38 +127,6 @@ class IterAgg[T](CommonBase[Iterable[T]]):
         10
         """
         return mit.argmin(self._data, key=key)
-
-    def is_sorted[U](
-        self,
-        key: Callable[[T], U] | None = None,
-        reverse: bool = False,
-        strict: bool = False,
-    ) -> bool:
-        """
-        Returns True if the items of iterable are in sorted order, and False otherwise.
-
-        Key and reverse have the same meaning that they do in the built-in sorted function.
-
-        >>> from pychain import Iter
-        >>> Iter(["1", "2", "3", "4", "5"]).is_sorted(key=int)
-        True
-        >>> Iter([5, 4, 3, 1, 2]).is_sorted(reverse=True)
-        False
-
-        If strict, tests for strict sorting, that is, returns False if equal elements are found:
-
-        >>> Iter([1, 2, 2]).is_sorted()
-        True
-        >>> Iter([1, 2, 2]).is_sorted(strict=True)
-        False
-
-        The function returns False after encountering the first out-of-order item.
-
-        This means it may produce results that differ from the built-in sorted function for objects with unusual comparison dynamics (like math.nan).
-
-        If there are no out-of-order items, the iterable is exhausted.
-        """
-        return mit.is_sorted(self._data, key=key, reverse=reverse, strict=strict)
 
     def sum[U: int | float](self: CommonBase[Iterable[U]]) -> U | Literal[0]:
         """
---
+++ src/pychain/_iter/_booleans.py
@@ -0,0 +1,124 @@
+from collections.abc import Callable, Iterable
+
+import cytoolz as cz
+import more_itertools as mit
+
+from .._core import CommonBase
+
+
+class IterBool[T](CommonBase[Iterable[T]]):
+    _data: Iterable[T]
+
+    def is_not_empty(self) -> bool:
+        """
+        Return True if the iterable is not empty.
+
+            >>> from pychain import Iter
+            >>> Iter([1]).is_not_empty()
+            True
+        """
+        return True if self._data else False
+
+    def is_empty(self) -> bool:
+        """
+        Return True if the iterable is empty.
+
+            >>> from pychain import Iter
+            >>> Iter([]).is_empty()
+            True
+        """
+        return False if self._data else True
+
+    def is_distinct(self) -> bool:
+        """
+        Return True if all items are distinct.
+
+            >>> from pychain import Iter
+            >>> Iter([1, 2]).is_distinct()
+            True
+        """
+        return cz.itertoolz.isdistinct(self._data)
+
+    def all(self) -> bool:
+        """
+        Return True if all items are truthy.
+
+            >>> from pychain import Iter
+            >>> Iter([1, True]).all()
+            True
+        """
+        return all(self._data)
+
+    def any(self) -> bool:
+        """
+        Return True if any item is truthy.
+
+            >>> from pychain import Iter
+            >>> Iter([0, 1]).any()
+            True
+        """
+        return any(self._data)
+
+    def all_equal[U](self, key: Callable[[T], U] | None = None) -> bool:
+        """
+        Return True if all items are equal.
+
+            >>> from pychain import Iter
+            >>> Iter([1, 1, 1]).all_equal()
+            True
+        """
+        return mit.all_equal(self._data, key=key)
+
+    def all_unique[U](self, key: Callable[[T], U] | None = None) -> bool:
+        """
+        Returns True if all the elements of iterable are unique (no two elements are equal).
+
+        >>> from pychain import Iter
+        >>> Iter("ABCB").all_unique()
+        False
+
+        If a key function is specified, it will be used to make comparisons.
+
+        >>> Iter("ABCb").all_unique()
+        True
+
+        >>> Iter("ABCb").all_unique(str.lower)
+        False
+
+        The function returns as soon as the first non-unique element is encountered.
+        Iterables with a mix of hashable and unhashable items can be used, but the function will be slower for unhashable items
+
+        """
+        return mit.all_unique(self._data, key=key)
+
+    def is_sorted[U](
+        self,
+        key: Callable[[T], U] | None = None,
+        reverse: bool = False,
+        strict: bool = False,
+    ) -> bool:
+        """
+        Returns True if the items of iterable are in sorted order, and False otherwise.
+
+        Key and reverse have the same meaning that they do in the built-in sorted function.
+
+        >>> from pychain import Iter
+        >>> Iter(["1", "2", "3", "4", "5"]).is_sorted(key=int)
+        True
+        >>> Iter([5, 4, 3, 1, 2]).is_sorted(reverse=True)
+        False
+
+        If strict, tests for strict sorting, that is, returns False if equal elements are found:
+
+        >>> Iter([1, 2, 2]).is_sorted()
+        True
+        >>> Iter([1, 2, 2]).is_sorted(strict=True)
+        False
+
+        The function returns False after encountering the first out-of-order item.
+
+        This means it may produce results that differ from the built-in sorted function for objects with unusual comparison dynamics (like math.nan).
+
+        If there are no out-of-order items, the iterable is exhausted.
+        """
+        return mit.is_sorted(self._data, key=key, reverse=reverse, strict=strict)
