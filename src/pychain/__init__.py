b23fdca62609abafe9b48daf1ed45ce0b4c10c70
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-10-17 17:25:02 +0200

    updating public interface and addinge xprs example/test

--- src/pychain/__init__.py
+++ src/pychain/__init__.py
@@ -1,5 +1,5 @@
 from ._core import Wrapper
-from ._dict import Dict
+from ._dict import Dict, key
 from ._iter import Iter
 
-__all__ = ["Dict", "Iter", "Wrapper"]
+__all__ = ["Dict", "Iter", "Wrapper", "key"]
