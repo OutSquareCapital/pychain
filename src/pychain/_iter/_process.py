4b0b08f9052de2bcef7e1340ddb556658450f6bf
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-10-22 14:22:52 +0200

    improved/fixed random sample doc

--- src/pychain/_iter/_process.py
+++ src/pychain/_iter/_process.py
@@ -43,10 +43,33 @@ class BaseProcess[T](IterWrapper[T]):
         self, probability: float, state: Random | int | None = None
     ) -> Iter[T]:
         """
-        Randomly sample items with given probability.
+        Return elements from a sequence with probability of prob
+
+        Returns a lazy iterator of random items from seq.
+
+        random_sample considers each item independently and without replacement.
+
+        See below how the first time it returned 13 items and the next time it returned 6 items.
         >>> import pychain as pc
-        >>> len(Iterable(Iter([1, 2, 3]).random_sample(0.5)))  # doctest: +SKIP
-        1
+        >>> seq = list(range(100))
+        >>> pc.Iter(seq).random_sample(0.1).into(list)  # doctest: +SKIP
+        [6, 9, 19, 35, 45, 50, 58, 62, 68, 72, 78, 86, 95]
+        >>> pc.Iter(seq).random_sample(0.1).into(list)  # doctest: +SKIP
+        [6, 44, 54, 61, 69, 94]
+
+        Providing an integer seed for random_state will result in deterministic sampling.
+
+        Given the same seed it will return the same sample every time.
+        >>> pc.Iter(seq).random_sample(0.1, state=2016).into(list)
+        [7, 9, 19, 25, 30, 32, 34, 48, 59, 60, 81, 98]
+        >>> pc.Iter(seq).random_sample(0.1, state=2016).into(list)
+        [7, 9, 19, 25, 30, 32, 34, 48, 59, 60, 81, 98]
+
+        random_state can also be any object with a method random that returns floats between 0.0 and 1.0 (exclusive).
+        >>> from random import Random
+        >>> randobj = Random(2016)
+        >>> pc.Iter(seq).random_sample(0.1, state=randobj).into(list)
+        [7, 9, 19, 25, 30, 32, 34, 48, 59, 60, 81, 98]
         """
 
         return self.apply(
